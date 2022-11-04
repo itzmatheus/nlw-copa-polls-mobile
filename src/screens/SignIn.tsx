33c5c41804e9fa6b1139803ff95a832fa8fabcb1
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-03 22:20:44 -0300

    feat: customize sigin

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,35 @@
-import { Center, Text } from "native-base"
+import { useState } from "react";
+import { Center, Text, Icon } from "native-base"
+import { Fontisto } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+import { Button } from "../components/Button";
 
 export function SignIn() {
+
+    const [isLoading, setIsLoading] = useState(false)
+
     return (
-        <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24} fontFamily="heading" >SignIn</Text>
+        <Center flex={1} bgColor="gray.900" p={7}>
+            <Logo width={212} height={40} />
+            <Button
+                title="ENTRAR COM GOOGLE"
+                type="SECONDARY"
+                isLoading={isLoading}
+                mt={12}
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>
+                }
+                onTouchEndCapture={() => setIsLoading(!isLoading)}
+            />
+
+            <Text
+                color="white"
+                textAlign="center"
+                mt={4}
+            >
+                Não utilizamos nenhuma informação além {'\n'}
+                do seu e-mail para criação de sua conta.
+            </Text>
         </Center>
-    )
+    ) 
 }
