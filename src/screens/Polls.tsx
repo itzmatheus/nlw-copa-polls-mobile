10ed0e274f4dea6c4f29ec51d666d62a93f65014
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-05 19:06:47 -0300

    feat: add list own polls

--- src/screens/Polls.tsx
+++ src/screens/Polls.tsx
@@ -1,14 +1,48 @@
-import { useNavigation } from '@react-navigation/native';
-import { VStack, Icon } from 'native-base';
+import { useCallback, useState } from 'react';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { VStack, Icon, useToast, FlatList } from 'native-base';
 import { Octicons } from '@expo/vector-icons'
+
+import { api } from '../services/api';
+
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
-
+import { Loading } from '../components/Loading';
+import { PollCard, PollCardProps } from '../components/PollCard';
+import { EmptyPollList } from '../components/EmptyPollList';
 
 
 export function Polls() {
 
     const { navigate } = useNavigation();
+
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ polls, setPolls ] = useState<PollCardProps[]>([]);
+
+    const toast = useToast();
+
+    async function fetchPolls() {
+
+        try {
+            setIsLoading(true)
+            const response = await api.get('/polls');
+            setPolls(response.data.polls)
+        } catch (error) {
+            console.log(error)
+            toast.show({
+                title: 'Não foi possível carregar os bolões',
+                placement: 'top',
+                bgColor: 'red.500',
+            })
+        } finally {
+            setIsLoading(false)
+        }
+
+    }
+
+    useFocusEffect(useCallback(() => {
+        fetchPolls();
+    }, []));
 
     return (
         <VStack flex={1} bgColor="gray.900">
@@ -22,6 +56,19 @@ export function Polls() {
                     onPress={() => navigate('find')}
                 />
             </VStack>
+
+            {
+                isLoading ? <Loading /> :
+                <FlatList
+                    data={polls}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <PollCard data={item} />}
+                    px={5}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ pb: 32 }}
+                    ListEmptyComponent={() => <EmptyPollList />}
+                />
+            }
         </VStack>
     );
 }
