088940adf9f6ad3c9725bd87177f5b3862328ed1
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-05 15:09:08 -0300

    feat: add axios api, implements login feature and verify if user is logged in

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
+import { api } from "../services/api";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -26,29 +27,41 @@ export const AuthContext = createContext({} as AuthContextDataProps);
 export function AuthContextProvider({ children }: AuthProviderProps) {
 
     const [user, setUser] = useState<UserProps>({} as UserProps);
-    const [isUserLoading, setIsLoading] = useState(false);
+    const [isUserLoading, setIsUserLoading] = useState(false);
 
     const [request, response, promptAsync] = Google.useAuthRequest({
-        clientId: '',
+        clientId: 'YOUR_CLIENT_ID_GOOGLE',
         redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
         scopes: ['profile', 'email']
     });
 
     async function signInWithGoogle(access_token: string) {
-        console.log('TOKEN DE AUTENTICACAO ===> '+ access_token);
+        try {
+            setIsUserLoading(true);
+            const tokenResponse = await api.post('/users', { access_token })
+            api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`
+
+            const userInfoResonse = await api.get('/me');
+            setUser(userInfoResonse.data.user);
+        } catch (err) {
+            console.log('Erro SignIn: ' + err);
+            throw err;
+        } finally {
+            setIsUserLoading(false);
+        }
     }
 
     async function signIn() {
 
         try {
-            setIsLoading(true);
+            setIsUserLoading(true);
             await promptAsync();
 
         } catch (error) {
             console.log(error);
             throw error;
         } finally {
-            setIsLoading(false);
+            setIsUserLoading(false);
         }
 
     }
